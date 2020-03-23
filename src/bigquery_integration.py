21fbd287dc42454f24aff1359176e721421a9936
Salik Sadruddin <salik.merani@gmail.com>
2020-03-23 22:43:04 +0500

    added delay

--- src/bigquery_integration.py
+++ src/bigquery_integration.py
@@ -3,6 +3,7 @@ import glob
 from google.cloud import bigquery
 import csv
 import os
+import time
 
 bq = bigquery.Client()
 base_path = os.getcwd()
@@ -10,6 +11,11 @@ base_path = os.getcwd()
 for file in glob.glob(pathname=os.path.abspath(base_path+'/../data')+'/memes_comments*.log', recursive=False):
   with open(file) as dfile:
     rd = csv.reader(dfile, delimiter="\t", quotechar='"')
+    i = 0
     for row in rd:
       query = "INSERT INTO memonavirus.comments (timestamp, comment_author, comment_id, parent_author, parent_id, comment, infected) VALUES('{}', '{}', '{}', '{}', '{}', {}, {})".format(row[0], row[1], row[2], row[3], row[4], "true" if row[5] == "C" else "false", "true" if row[6] == "I" else "false")
       result = bq.query(query).result()
+      i += 1
+      if i % 5 == 0:
+        time.sleep(5)
+      
