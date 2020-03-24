28cb70ac98f3de7f152266f9aeaaecc7fce01e27
Salik Sadruddin <salik.merani@gmail.com>
2020-03-24 14:49:14 +0500

    functionized data

--- src/bigquery_integration.py
+++ src/bigquery_integration.py
@@ -1,31 +1,41 @@
 # load all data into BigQuery
 import glob
 from google.cloud import bigquery
+import sys
 import csv
 import os
 import time
 
 bq = bigquery.Client()
+
 base_path = os.getcwd()
 
-for file in glob.glob(pathname=os.path.abspath(base_path+'/../data')+'/memes_comments*.log', recursive=False):
+def big_query_save():
+  file_path = sys.argv[1]
+  infected = True if file_path.find("infections") != -1 else False
+  file = os.path.abspath(base_path+"/"+file_path)
   with open(file) as dfile:
     rd = csv.reader(dfile, delimiter="\t", quotechar='"')
     i = 0
-    for row in rd:
-      query = "INSERT INTO memonavirus.comments (timestamp, comment_author, comment_id, parent_author, parent_id, comment, infected) VALUES('{}', '{}', '{}', '{}', '{}', {}, {})".format(row[0], row[1], row[2], row[3], row[4], "true" if row[5] == "C" else "false", "true" if row[6] == "I" else "false")
-      result = bq.query(query).result()
+    for row in rd: 
+      query = ""
+      if infected:
+        query = """INSERT INTO memonavirus.comments (timestamp, comment_author, comment_id, parent_author, parent_id, comment, infected) 
+        VALUES('{}', '{}', '{}', '{}', '{}', {}, {})""".format(row[0], row[1], row[2], row[3], row[4], "true" if row[5] == "C" else "false", "true" if row[6] == "I" else "false")
+      else:
+        query = """INSERT INTO memonavirus.infections (timestamp, comment_author, comment_id, infected_by_author, infected_by_id, comment) 
+        VALUES('{}', '{}', '{}', '{}', '{}', {})""".format(row[0], row[1], row[2], row[3], row[4], "true" if row[5] == "C" else "false")
+      bq.query(query).result()
       i += 1
       if i % 5 == 0:
         time.sleep(5)
-      
-for file in glob.glob(pathname=os.path.abspath(base_path+'/../data')+'/memes_infections*.log', recursive=False):
-  with open(file) as dfile:
-    rd = csv.reader(dfile, delimiter="\t", quotechar='"')
-    i = 0
-    for row in rd:
-      query = "INSERT INTO memonavirus.infections (timestamp, comment_author, comment_id, infected_by_author, infected_by_id, comment) VALUES('{}', '{}', '{}', '{}', '{}', {})".format(row[0], row[1], row[2], row[3], row[4], "true" if row[5] == "C" else "false")
-      result = bq.query(query).result()
-      i += 1
-      if i % 5 == 0:
-        time.sleep(5)
+
+def bigquery_save_comments(timestamp, comment_author, comment_id, parent_author, parent_id, comment, infected):
+  query = """INSERT INTO memonavirus.comments (timestamp, comment_author, comment_id, parent_author, parent_id, comment, infected) 
+    VALUES('{}', '{}', '{}', '{}', '{}', {}, {})""".format(timestamp, comment_author, comment_id, parent_author, parent_id, "true" if comment == "C" else "false", "true" if infected == "I" else "false")
+  bq.query(query).result()
+
+def bigquery_save_infected(timestamp, comment_author, comment_id, infected_by_author, infected_by_id, comment):
+  query = """INSERT INTO memonavirus.infections (timestamp, comment_author, comment_id, infected_by_author, infected_by_id, comment) 
+  VALUES('{}', '{}', '{}', '{}', '{}', {})""".format(timestamp, comment_author, comment_id, infected_by_author, infected_by_id, "true" if comment == "C" else "false")
+  bq.query(query).result()
